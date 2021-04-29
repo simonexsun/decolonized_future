871febcd697d26a43822bb335c0ff6608e4ba81f
Simone Sun <sunx660@newschool.edu>
2021-04-28 21:10:17 -0400

    Clean up code

--- js/main.js
+++ js/main.js
@@ -1,82 +1,84 @@
-    // Find the latest version by visiting https://cdn.skypack.dev/three.
-    import * as THREE from 'https://cdn.skypack.dev/three@0.128.0';
-    import { OBJLoader } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/loaders/OBJLoader.js';
-    import { MTLLoader } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/loaders/MTLLoader.js';
-    import { OrbitControls } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js';
+// Find the latest version by visiting https://cdn.skypack.dev/three.
+import * as THREE from 'https://cdn.skypack.dev/three@0.128.0';
+import { OBJLoader } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/loaders/OBJLoader.js';
+import { MTLLoader } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/loaders/MTLLoader.js';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js';
 
-    function main() {
-        const canvas = document.querySelector('#c');
-        const renderer = new THREE.WebGLRenderer({ canvas });
+function main() {
+    const canvas = document.querySelector('#c');
+    const renderer = new THREE.WebGLRenderer({ canvas });
 
-        const fov = 45;
-        const aspect = 2;  // the canvas default
-        const near = 0.1;
-        const far = 100;
-        const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-        camera.position.set(0, 10, 20);
+    //camera attributes
+    const fov = 45;
+    const aspect = 2;  // the canvas default
+    const near = 0.1;
+    const far = 100;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.set(0, 10, 20);
 
-        const controls = new OrbitControls(camera, canvas);
-        controls.target.set(0, 5, 0);
-        controls.update();
+    //control attributes
+    const controls = new OrbitControls(camera, canvas);
+    controls.target.set(0, 5, 0);
+    controls.update();
 
-        const scene = new THREE.Scene();
-        scene.background = new THREE.Color('black');
+    //background attributes
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color('black');
 
-        {
-            const skyColor = 0xB1E1FF;  // light blue
-            const groundColor = 0xB97A20;  // brownish orange
-            const intensity = 1;
-            const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
-            scene.add(light);
+    //sky attributes, must use {}
+    {
+        const skyColor = 0xB1E1FF;  // light blue
+        const groundColor = 0xB97A20;  // brownish orange
+        const intensity = 1;
+        const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+        scene.add(light);
+    }
+    
+    //lighting attributes, must use {}
+    {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(0, 10, 0);
+        light.target.position.set(-5, 0, 0);
+        scene.add(light);
+        scene.add(light.target);
+    }
+
+    //load 3D model files
+    const objLoader = new OBJLoader();
+    const mtlLoader = new MTLLoader();
+    mtlLoader.load('src/nature.vox.mtl', function (materials) {
+        materials.preload();
+        objLoader.setMaterials(materials);
+        objLoader.load('src/nature.vox.obj', function (object) {
+            scene.add(object);
+        });
+    });
+
+    //renderer attributes
+    function resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+        renderer.setSize(width, height, false);
         }
+        return needResize;
+    }
 
-        {
-            const color = 0xFFFFFF;
-            const intensity = 1;
-            const light = new THREE.DirectionalLight(color, intensity);
-            light.position.set(0, 10, 0);
-            light.target.position.set(-5, 0, 0);
-            scene.add(light);
-            scene.add(light.target);
-        }
-
-        {
-            const objLoader = new OBJLoader();
-            const mtlLoader = new MTLLoader();
-            mtlLoader.load('src/nature.vox.mtl', function (materials) {
-            materials.preload();
-            objLoader.setMaterials(materials);
-            objLoader.load('src/nature.vox.obj', function (object) {
-                scene.add(object);
-            });
-            });
-        }
-
-        function resizeRendererToDisplaySize(renderer) {
-            const canvas = renderer.domElement;
-            const width = canvas.clientWidth;
-            const height = canvas.clientHeight;
-            const needResize = canvas.width !== width || canvas.height !== height;
-            if (needResize) {
-            renderer.setSize(width, height, false);
-            }
-            return needResize;
-        }
-
-        function render() {
-
-            if (resizeRendererToDisplaySize(renderer)) {
+    function render() {
+        if (resizeRendererToDisplaySize(renderer)) {
             const canvas = renderer.domElement;
             camera.aspect = canvas.clientWidth / canvas.clientHeight;
             camera.updateProjectionMatrix();
-            }
-
-            renderer.render(scene, camera);
-
-            requestAnimationFrame(render);
         }
-
+        renderer.render(scene, camera);
         requestAnimationFrame(render);
     }
 
-    main();
+    requestAnimationFrame(render);
+}
+
+main();
